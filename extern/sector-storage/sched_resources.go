8e53c50af90c52252557bc4cf709443c3e071ede
long568 <zandzx02cn@qq.com>
2021-05-30 01:57:30 +0000

    lo: 20210530

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -103,7 +103,7 @@ func (a *activeResources) canHandleRequest(needRes Resources, wid WorkerID, call
 	// Added by long 20210405 -------------------------------------------------
 	switch needRes.taskType {
 	case sealtasks.TTAddPiece:
-		if a.p1ParallelNum >= LO_P1_PARALLEL_NUM {
+		if a.p1ParallelNum > 0 || a.p1ParallelNum >= LO_P1_PARALLEL_NUM {
 			// When the worker was filled by P1, there is no need to get AP.
 			log.Debugf("sched[AP]: not scheduling on worker %s for %s; P1ParallelNum get max", wid, caller)
 			return false
